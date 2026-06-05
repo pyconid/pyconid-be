a3cd42943798ef2f12f9a7fd7b08ffb3cdfc84f4
BimaAdi <bimaadi419@gmail.com>
2026-06-05 22:18:17 +0700

    add feature ticket order

--- repository/ticket.py
+++ repository/ticket.py
@@ -4,7 +4,7 @@ from models.Ticket import Ticket
 
 
 def get_active_tickets(db: Session):
-    return db.query(Ticket).filter(Ticket.is_active).all()
+    return db.query(Ticket).filter(Ticket.is_active).order_by(Ticket.order.asc()).all()
 
 
 def get_active_ticket_by_id(db: Session, ticket_id: str):
